f5cfb0d3bd3bc5d27e79a36684f746edd16dd357
Jose Gonzalez Gonzalez <Joseglez81164@gmail.com>
2026-07-29 17:45:15 +0100

    Create index.ts

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,7 @@
+// Componentes UI reutilizables de El Cénit
+export { Button } from './components/Button';
+export { Card } from './components/Card';
+export { Input } from './components/Input';
+export { Label } from './components/Label';
+export { Badge } from './components/Badge';
+export { cn } from './lib/utils';
