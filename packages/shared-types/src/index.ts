753c1958e4c04058498e61127797f2ad36528d88
Jose Gonzalez Gonzalez <Joseglez81164@gmail.com>
2026-07-29 18:06:20 +0100

    Create index.ts

---
+++ packages/shared-types/src/index.ts
@@ -0,0 +1,7 @@
+// Tipos compartidos de El Cénit - Testamento Digital
+
+export * from './testamento';
+export * from './heredero';
+export * from './bien';
+export * from './firma';
+export * from './usuario';
