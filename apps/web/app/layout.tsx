77f3de46ae264c6b03e11673e207b0b8aac24407
Jose Gonzalez Gonzalez <Joseglez81164@gmail.com>
2026-08-04 10:42:06 +0100

    Update layout.tsx

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, Merriweather } from 'next/font/google';
 import '../styles/globals.css';
 import { Toaster } from 'sonner';
 import { Navbar } from '@/components/layout/Navbar';
+import { SessionProvider } from '@/components/providers/SessionProvider';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 const merriweather = Merriweather({ weight: ['400', '700'], subsets: ['latin'], variable: '--font-merriweather' });
@@ -24,11 +25,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="es" className={`${inter.variable} ${merriweather.variable}`}>
       <body className="min-h-screen bg-background font-sans antialiased">
-        <Navbar />
-        <main className="relative flex min-h-screen flex-col">
-          {children}
-        </main>
-        <Toaster position="top-right" richColors />
+        <SessionProvider>
+          <Navbar />
+          <main className="relative flex min-h-screen flex-col">
+            {children}
+          </main>
+          <Toaster position="top-right" richColors />
+        </SessionProvider>
       </body>
     </html>
   );
